dae031b097f90f68c5ada76ac9353ed9af3a8d1b
noaarobles <roblesnoe1030@gmail.com>
2022-11-02 18:24:11 -0500

    added reset.css and modified js

--- todo.js
+++ todo.js
@@ -13,48 +13,114 @@ let arrayOfTodos = [
         "title": "delectus aut autem",
         "completed": false
     }]
-let filterTodos 
+
+let ol = document.getElementById("todo-list")
+
+
+
+// this will fetch the array from the json//
 const fetchTodos = () => {
     fetch('https://jsonplaceholder.typicode.com/todos')
         .then((response) => response.json())
-        .then((json) => arrayOfTodos = json)
+        .then((json) => (arrayOfTodos = json))
 }
 
+
+// this will log the array//
 const logTodos = () => {
     console.log(arrayOfTodos)
 }
 
-let ol = document.getElementById("todo-list")
-const populateTodos = () => {
-    // get elements to put todos in
-    // loop thru arrays of todos
-    for (let i = 0; i < arrayOfTodos.length; i++) {
-        // create element to make a new li
-        let listItem = document.createElement("li")
-        console.log(listItem)
-        let listItemText = document.createTextNode(arrayOfTodos[i].title)
 
+// this clear the list //
+const clearList = () => {
 
-        // create text node to put title property
-
-        // append text to li element
-        listItem.appendChild(listItemText)
-
-
-        //append li element to ol
-        ol.appendChild(listItem)
-    }
+    ol.remove();
+    let liContainer = document.getElementById("listContainer")
+    liContainer.innerHTML = "<ol id='todo-list'></ol>"
+    ol = document.getElementById("todo-list")
 
 }
+
+
+
+// this will populate the list//
+const populateTodos = () => {
+
+    clearList();
+
+    for (let i = 0; i < arrayOfTodos.length; i++) {
+        let newListItem = document.createElement("LI")
+        newListItem.innerText = arrayOfTodos[i].title
+        ol.appendChild(newListItem)
+    }
+}
+
+// this will filter the toDos//
 const filterTodos = () => {
+
+
     let userIdElement = document.getElementById("userId")
-    let userIdValue = userIdElement.value
-    console.log('userIdElement:', [userIdElement])
-    consoile.log('userIdValue:', userIdValue)
-    
-    ol.innerHTML = null
-    filterTodos.filter((todo) => {
-        return todo.userId === userIdValue
+    console.log("UserIdElement:", userIdElement)
+
+
+    let userIdValue = userIdElement.value;
+    console.log("userIdValue:", userIdValue)
+
+
+    let filterTodos = arrayOfTodos.filter((todo) => {
+        console.log(todo.userId == userIdValue)
+        return todo.userId == userIdValue
     })
-    console.log('filterTodos:', filterTodos)
+
+    console.log("filterTodos:", filterTodos)
+
+    for (let i = 0; i < filterTodos.length; i++) {
+        let newListItem = document.createElement("LI")
+        newListItem.innerText = filterTodos[i].title
+        ol.appendChild(newListItem)
+    }
+}
+
+// this is for the completed button//
+const completed = () => {
+
+    clearList();
+
+    let complete = arrayOfTodos.filter((todo) => {
+        console.log(todo.completed == true)
+        return todo.completed == true
+    })
+
+    console.log("complete:", complete)
+
+    for (let i = 0; i < complete.length; i++) {
+        let newListItem = document.createElement("LI")
+        newListItem.innerText = complete[i].title
+        ol.appendChild(newListItem)
+    }
+}
+
+
+
+//   this is for the incomplete button//
+const incompleted = () => {
+
+    clearList()
+
+    let incomplete = arrayOfTodos.filter((todo) => {
+        console.log(todo.completed == false)
+        return todo.completed == false
+    })
+
+
+    console.log("Incomplete:", incomplete)
+
+
+
+    for (let i = 0; i < incomplete.length; i++) {
+        let newListItem = document.createElement("LI")
+        newListItem.innerText = incomplete[i].title
+        ol.appendChild(newListItem)
+    }
 }
